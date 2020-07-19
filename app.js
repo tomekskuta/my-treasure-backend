10b7c5cf2162989997a3a984903f8d89327a1de0
tomekskuta <kontakt@tomekskuta.pl>
2020-07-19 21:45:49 +0200

    add cors middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 require('./store')
 
@@ -9,6 +10,7 @@ const gameRouter = require('./routes/game')
 const app = express()
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
